4242541496f4c2ced129744e422f9867b6addb2d
user134438 <user134438@77-240-12-9.rdns.melbourne.co.uk>
2018-02-21 11:01:27 -0800

    initial commit

--- helloworld_xcode/ViewController.swift
+++ helloworld_xcode/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
